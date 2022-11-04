4e0f6bfa63bb406f6c3b6987ce90e584299061c6
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-11-04 16:38:23 +0000

    Support styleImport fields

--- packages/host-portal-app/src/components/remote-view.tsx
+++ packages/host-portal-app/src/components/remote-view.tsx
@@ -1,5 +1,4 @@
 import React, {
-  ComponentType,
   createContext,
   PropsWithChildren,
   useContext,
@@ -10,55 +9,69 @@ import React, {
 import { Manifest, View } from '../types';
 
 const loading = Symbol('loading');
-type MicroFrontendState = Record<string, React.ComponentType | typeof loading>;
 
-const views = createContext<
-  [
+type MicroFrontendState = Record<string, React.ComponentType | typeof loading>;
+type GlobalStylesheetSet = Set<string>;
+type RemoteViewsContext = {
+  microFrontends: [
     MicroFrontendState,
     (value: (prevState: MicroFrontendState) => MicroFrontendState) => void,
-  ]
->([{}, () => null]);
+  ];
+  globalStyles: [
+    GlobalStylesheetSet,
+    (value: (prevState: GlobalStylesheetSet) => GlobalStylesheetSet) => void,
+  ];
+};
 
-async function loadRemoteView(baseUrl: string): Promise<ComponentType | void> {
-  const response = await fetch(`${baseUrl}/package.json`);
-  const manifest = (await response.json()) as Manifest;
-
-  // TODO: check for (and eventually load) global CSS
-
-  // Load microfrontend's local style
-  if (manifest.style) {
-    document.head.insertAdjacentHTML(
-      'beforeend',
-      `<link rel='stylesheet' href='${baseUrl}${manifest.style}' />`,
-    );
-  }
-
-  // Dynamically import ESM entrypoint
-  if (manifest.module) {
-    const { default: LoadedView } = (await import(
-      /* webpackIgnore: true */ `${baseUrl}${manifest.module}`
-    )) as View;
-
-    return LoadedView;
-  }
-}
+export const viewContext = createContext<RemoteViewsContext>(
+  {} as RemoteViewsContext,
+);
 
 export const useRemoteView = (baseUrl: string): React.ComponentType | null => {
-  const [state, setState] = useContext(views);
-  const current = state[baseUrl];
+  const {
+    microFrontends: [microFrontendsState, setMicroFrontendsState],
+    globalStyles: [globalStylesState, setGlobalStylesState],
+  } = useContext(viewContext);
+
+  const current = microFrontendsState[baseUrl];
 
   useEffect(() => {
-    if (current !== undefined) {
-      return;
+    if (current === undefined) {
+      void loadRemoteView();
     }
 
-    void loadRemoteView(baseUrl).then((LoadedView) => {
-      LoadedView && setState((old) => ({ ...old, [baseUrl]: LoadedView }));
-    });
-  }, [current, baseUrl, setState]);
+    async function loadRemoteView() {
+      const manifest = await loadRemoteManifest(baseUrl);
+
+      // Load local CSS via manifest "style" field
+      if (manifest.style) {
+        injectRemoteCss(`${baseUrl}${manifest.style}`);
+      }
+
+      // Load global (CDN) CSS via manifest "styleImports" field and make sure it's not been already injected in the page
+      manifest.styleImports?.forEach((url) => {
+        if (!globalStylesState.has(url)) {
+          setGlobalStylesState((globalStyles) => globalStyles.add(url));
+          injectRemoteCss(url);
+        }
+      });
+
+      // Load module
+      const LoadedView = await importRemoteModule(
+        `${baseUrl}${manifest.module}`,
+      );
+      setMicroFrontendsState((prev) => ({ ...prev, [baseUrl]: LoadedView }));
+    }
+  }, [
+    current,
+    baseUrl,
+    setMicroFrontendsState,
+    globalStylesState,
+    setGlobalStylesState,
+  ]);
 
   if (current === undefined) {
-    setState((prev) => ({ ...prev, [baseUrl]: loading }));
+    setMicroFrontendsState((prev) => ({ ...prev, [baseUrl]: loading }));
     return null;
   }
 
@@ -72,11 +85,37 @@ export const useRemoteView = (baseUrl: string): React.ComponentType | null => {
 export const RemoteViews = ({
   children,
 }: PropsWithChildren<unknown>): JSX.Element => {
-  const value = useState<MicroFrontendState>({});
-  return <views.Provider value={value}>{children}</views.Provider>;
+  const microFrontends = useState<MicroFrontendState>({});
+  const globalStyles = useState<GlobalStylesheetSet>(new Set());
+  return (
+    <viewContext.Provider value={{ microFrontends, globalStyles }}>
+      {children}
+    </viewContext.Provider>
+  );
 };
 
 export const RemoteView = ({ baseUrl }: { baseUrl: string }): JSX.Element => {
   const ViewComponent = useRemoteView(baseUrl);
   return (ViewComponent && <ViewComponent />) || <div>Loading</div>;
 };
+
+function injectRemoteCss(url: string) {
+  document.head.insertAdjacentHTML(
+    'beforeend',
+    `<link rel='stylesheet' href='${url}' />`,
+  );
+}
+
+async function loadRemoteManifest(baseUrl: string) {
+  const response = await fetch(`${baseUrl}/package.json`);
+  const manifest = (await response.json()) as Manifest;
+  return manifest;
+}
+
+async function importRemoteModule(url: string) {
+  const { default: LoadedView } = (await import(
+    /* webpackIgnore: true */ url
+  )) as View;
+
+  return LoadedView;
+}
